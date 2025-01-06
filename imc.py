a8962c61d2bda57dba53d3706c5303a0a996b5d2
Excelsior8Elpharah <excelsior8Elpharah@gmail.com>
2025-01-06 20:39:27 -0300

    “início do projeto de cálculo de massa corporal

---
+++ imc.py
@@ -0,0 +1,21 @@
+print("Cálculo de IMC)
+
+altura = float(input("Digite sua altura em metros: "))
+peso = float(input("Digite seu peso em Kg: "))
+
+imc = peso / (altura**2)
+
+if imc < 18.5:
+    categoria = "Baixo peso"
+elif imc < 25:
+    categoria = "Peso adequado"
+elif imc < 30:
+    categoria = "Sobrepeso"
+elif imc < 35:
+    categoria = "Obesidade Grau I"
+elif imc < 40:
+    categoria = "Obesidade Grau II"
+else:
+    categoria = "Obesidade Grau III"
+
+print(f"Seu IMC é {imc:.2f} e sua categoria é {categoria}")
